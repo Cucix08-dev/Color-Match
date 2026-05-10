0b014ad1daf7d4dc48cd0f098e4aa4a8a362bcea
Cucix08-dev <cucixdev@gmail.com>
2026-05-11 01:38:33 +0200

    try to fix 4

--- script.js
+++ script.js
@@ -26,14 +26,14 @@ document.addEventListener("DOMContentLoaded", () => {
             targetY = startY;
         }
 
-        path = "/src/howToPlay.html";
+        path = "/Color-Match/src/howToPlay.html";
     }
 
     if (isHowToPlay) {
         const savedY = Number(localStorage.getItem("homeButtonY"));
         targetY = Number.isFinite(savedY) ? savedY : startY;
 
-        path = "/../index.html";
+        path = "/Color-Match/../index.html";
         howToPlayButton.style.border = "none";
     }
 
